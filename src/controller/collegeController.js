a73c64ef8b35ff3667873cd42bf5c61598bbda62
manju349 <manjunath349@gmail.com>
2022-06-29 13:41:25 +0530

    get left

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -6,6 +6,42 @@ const createColleges = async function (req,res){
 
     try {
     let collegeData = req.body;
+    
+    // BODY VALIDATION
+    if (!validator.isValidRequestBody(collegeData)){
+        return res.send({status: false, msg: "college details required"})
+    }
+
+    // NAME VALIDATION
+    if (!validator.isValidField(collegeData.name)){
+        return res.send({status: false, msg: "Name is required"})
+    }
+    if (!validator.isValidName(collegeData.name)){
+        return res.send({status: false, msg: "Name is not valid"})
+    }
+
+    let collegeName = await collegeModel.findOne({name: collegeData.name})
+    if (collegeName){
+        return res.send({status: false, msg: "This name already exists"})
+    }
+
+    // FULL NAME VALIDATION
+    if (!validator.isValidField(collegeData.fullName)){
+        return res.send({status: false, msg: "Full Name is required"})
+    }
+    if (!validator.isValidName(collegeData.fullName)){
+        return res.send({status: false, msg: "Full Name is not valid"})
+    }
+
+    // LOGO LINK VALIDATION
+    if (!validator.isValidField(collegeData.logoLink)){
+        return res.send({status: false, msg: "Logo link is required"})
+    }
+    if (!validator.isValidURL(collegeData.logoLink)){
+        return res.send({status: false, msg: "Logo Link is Invalid"})
+    }
+
+    // CREATE COLLEGE DATA
     let newCollege = await collegeModel.create(collegeData);
     return res.status(201).send({status: true, data: newCollege})
     }
@@ -14,12 +50,25 @@ const createColleges = async function (req,res){
     }
 }
 
-
+// GET LIST OF INTERNS BY COLLEGE
 const getCollegeDetails = async function (req,res){
-    let collegeName = req.query.collegeName
-    let data = await collegeModel.findOne({name: collegeName}).select({name: 1, fullName: 1, logoLink: 1, _id:0})
-    let interns = await internModel.find().select({_id:1, name: 1, email:1, mobile:1})
-    res.send({status: true, data: {data, interns}})
+    try{
+    let collegeNames = req.query.collegeName
+    let data = await collegeModel.findOne({name: collegeNames},{name: 1, fullName: 1, logoLink: 1})
+    
+    // let collegeId = data._id.toString()
+    console.log(data._id)
+    let interns = await internModel.find({collgeId: data._id})
+    //console.log(interns)
+    res.send({status: true, data: { data, interns}})
+
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send({status: false, msg: error.message})
+    }
+
+
 }
 
 module.exports.getCollegeDetails = getCollegeDetails
